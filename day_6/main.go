9e3e412c30a6a9da73475c5d5ef4b0dfad4a04e5
Jeroen de Jong <jeroen.dejong@nedap.com>
2023-11-16 22:04:33 +0100

    Add day 6

---
+++ day_6/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+var pattern = regexp.MustCompile(`(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)`)
+
+func parseLine(line string) (int, int, int, int, string) {
+	match := pattern.FindStringSubmatch(line)
+	x1, _ := strconv.Atoi(match[2])
+	y1, _ := strconv.Atoi(match[3])
+	x2, _ := strconv.Atoi(match[4])
+	y2, _ := strconv.Atoi(match[5])
+	return x1, y1, x2, y2, match[1]
+}
+
+func step1(x int, y int, action string, lights map[[2]int]int) map[[2]int]int {
+	switch action {
+	case "turn on":
+		lights[[2]int{x, y}] = 1
+	case "turn off":
+		lights[[2]int{x, y}] = 0
+	case "toggle":
+		lights[[2]int{x, y}] = 1 - lights[[2]int{x, y}]
+	}
+	return lights
+}
+
+func step2(x int, y int, action string, lights map[[2]int]int) map[[2]int]int {
+	switch action {
+	case "turn on":
+		lights[[2]int{x, y}] += 1
+	case "turn off":
+		lights[[2]int{x, y}] = max(0, lights[[2]int{x, y}]-1)
+	case "toggle":
+		lights[[2]int{x, y}] += 2
+	}
+	return lights
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	var lights = make(map[[2]int]int)
+
+	for _, line := range strings.Split(input, "\n") {
+		if string(line) == "" {
+			continue
+		}
+
+		x1, y1, x2, y2, action := parseLine(string(line))
+		for x := x1; x <= x2; x++ {
+			for y := y1; y <= y2; y++ {
+				if part == 1 {
+					lights = step1(x, y, action, lights)
+				} else {
+					lights = step2(x, y, action, lights)
+				}
+			}
+		}
+	}
+
+	var total = 0
+	for _, light := range lights {
+		total += light
+	}
+
+	fmt.Println("Answer: ", total)
+}
